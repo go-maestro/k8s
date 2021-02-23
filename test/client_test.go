7b2636c275446b200d2ce9a8fdc06666090a0f24
Thiago Henrique <contato@thiagohenrique.com.br>
2021-02-23 19:52:19 -0300

    tests: created some tests

---
+++ test/client_test.go
@@ -0,0 +1,86 @@
+package kubi_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/go-maestro/kubi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewClient(t *testing.T) {
+	t.Run("when KUBECONFIG points to a existing file", func(t *testing.T) {
+		pkgPath, _ := os.Getwd()
+		kubeconfig := fmt.Sprintf("%s/data/kubeconfig", pkgPath)
+
+		os.Setenv("KUBECONFIG", kubeconfig)
+
+		client, err := kubi.NewClient()
+
+		t.Run("shouldn't return error", func(t *testing.T) {
+			assert.Nil(t, err)
+		})
+
+		t.Run("should set client.RestConfig", func(t *testing.T) {
+			assert.Equal(t, client.RestConfig.Host, "https://0.0.0.0:46271")
+		})
+
+		os.Unsetenv("KUBECONFIG")
+	})
+
+	t.Run("when KUBECONFIG points to a non-existing file", func(t *testing.T) {
+		pkgPath, _ := os.Getwd()
+		kubeconfig := fmt.Sprintf("%s/i/doesn't/exists", pkgPath)
+
+		os.Setenv("KUBECONFIG", kubeconfig)
+
+		client, err := kubi.NewClient()
+
+		t.Run("shouldn't return the client", func(t *testing.T) {
+			assert.Nil(t, client)
+		})
+
+		t.Run("should return an error", func(t *testing.T) {
+			assert.Error(t, err)
+			assert.IsType(t, &os.PathError{}, err)
+		})
+
+		os.Unsetenv("KUBECONFIG")
+	})
+
+	t.Run("when KUBECONFIG wasn't set", func(t *testing.T) {
+		t.Run("when application is running inside Kubernetes", func(t *testing.T) {
+			assert.NotEmpty(t, os.Getenv("KUBERNETES_SERVICE_HOST"))
+			assert.NotEmpty(t, os.Getenv("KUBERNETES_SERVICE_PORT"))
+
+			assert.FileExists(t, "/var/run/secrets/kubernetes.io/serviceaccount/token")
+			assert.FileExists(t, "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+
+			client, err := kubi.NewClient()
+
+			t.Run("shouldn't return an error", func(t *testing.T) {
+				assert.NotNil(t, err)
+			})
+
+			t.Run("should return the client", func(t *testing.T) {
+				assert.IsType(t, &kubi.Client{}, client)
+			})
+		})
+
+		t.Run("when application isn't running inside Kubernetes", func(t *testing.T) {
+			os.Unsetenv("KUBERNETES_SERVICE_HOST")
+			os.Unsetenv("KUBERNETES_SERVICE_PORT")
+
+			client, err := kubi.NewClient()
+
+			t.Run("should return an error", func(t *testing.T) {
+				assert.Error(t, err)
+			})
+
+			t.Run("shouldn't return the client", func(t *testing.T) {
+				assert.Nil(t, client)
+			})
+		})
+	})
+}
